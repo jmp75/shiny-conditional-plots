f6924b1a1db1ca4654c7650e11623aac71e695a4
J-M <jean-michel.perraud@csiro.au>
2025-01-23 14:39:37 +1100

    Try to use modularisation of the code to generate plots. Issue with duplicated plots for two locations

--- basic-app/app.py
+++ basic-app/app.py
@@ -1,5 +1,7 @@
+from typing import Any
+
 import plotly.express as px
-from shiny import App, reactive, render, ui
+from shiny import App, module, reactive, render, ui
 from shinywidgets import output_widget, render_widget
 
 app_ui = ui.page_fluid(
@@ -7,12 +9,26 @@ app_ui = ui.page_fluid(
     ui.input_radio_buttons(
         id="station_id_rb",
         label=None,
-        choices=["123456", "999999"],
+        choices=["123456", "999999", "abcdef", "None"],
         inline=True,
     ),
     ui.output_ui("toggled_controls"),
 )
 
+# Adapting https://www.appsilon.com/post/shiny-for-python-clean-design-for-dynamic-plot-management
+@module.ui
+def plot_ui(stock_ticker:str, location:str) -> ui.TagChild:
+    return output_widget("plot")
+
+@module.server
+def plotly_stock_server(input, output, session, stock_ticker:str, location:str):
+    @render_widget # https://shiny.posit.co/py/components/outputs/plot-plotly/
+    def plot() -> Any:
+        df = px.data.stocks()
+        fig = px.line(df, x='date', y=stock_ticker, title=f"location: {location}")
+        return fig
+
+_current_location = None
 
 def server(input, output, session):
 
@@ -21,35 +37,45 @@ def server(input, output, session):
     @reactive.effect
     @reactive.event(input.station_id_rb)
     def update_selected_station():
+        global _current_location
         selected_station = input.station_id_rb()
         _current_location_rv.set(selected_station)
+        _current_location = selected_station
 
     @render.ui
     def toggled_controls():
+        global _current_location
         location = _current_location_rv.get()
-        if location is None:
+        location = _current_location
+        if location is None or location == "None":
             return ui.TagList(
                 ui.output_text("location_unknown")
             )
         else:
-            return ui.TagList(
-                output_widget("first_graph"),
-                output_widget("second_graph")
-            )
+            location=str(location)
+            stock_tickers = ['GOOG','AAPL']
+            for s in stock_tickers:
+                plotly_stock_server(f"plot_{s}", s, location)
+            tag_children=[plot_ui(f"plot_{s}", s, location) for s in stock_tickers]
+            return ui.TagList(tag_children)
+            # return ui.TagList(
+            #     output_widget("first_graph"),
+            #     output_widget("second_graph")
+            # )
 
-    @render_widget
-    def first_graph():
-        # Note: if one adds the following statement to the function body, then you get duplicated graph outputs
-        # location = _current_location_rv.get()
-        df = px.data.stocks()
-        fig = px.line(df, x='date', y="GOOG")
-        return fig
+    # @render_widget
+    # def first_graph():
+    #     # Note: if one adds the following statement to the function body, then you get duplicated graph outputs
+    #     # location = _current_location_rv.get()
+    #     df = px.data.stocks()
+    #     fig = px.line(df, x='date', y="GOOG")
+    #     return fig
 
-    @render_widget
-    def second_graph():
-        df = px.data.stocks()
-        fig = px.line(df, x='date', y="AAPL")
-        return fig
+    # @render_widget
+    # def second_graph():
+    #     df = px.data.stocks()
+    #     fig = px.line(df, x='date', y="AAPL")
+    #     return fig
 
     @render.text
     def location_unknown(): return "location_unknown"
